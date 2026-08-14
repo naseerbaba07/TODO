9a1d25fe0ec320acefb6fc0ff3e52d6d2db9db3e
Shaik Naseer Baba <23eg105v15@anurag.edu.in>
2026-08-15 01:08:08 +0530

    Update API base URLs to new endpoints

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://todo-1-avon.onrender.com/todos"
+  baseURL: "https://todo-1-fy7b.onrender.com/todos"
 });
 export const CHAT_API =
-    "https://todo-1-avon.onrender.com/chat";
+    "https://todo-1-fy7b.onrender.com/chat";
 
 export default API;
