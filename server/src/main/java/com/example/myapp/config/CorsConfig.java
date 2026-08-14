1dd10d319934e423eac26d268a8dd84c40651428
Shaik Naseer Baba <23eg105v15@anurag.edu.in>
2026-08-15 00:26:40 +0530

    Update CORS configuration to allow new origin

--- server/src/main/java/com/example/myapp/config/CorsConfig.java
+++ server/src/main/java/com/example/myapp/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173"); // your React site
+        config.addAllowedOrigin("https://babalist.vercel.app/"); // your React site
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
